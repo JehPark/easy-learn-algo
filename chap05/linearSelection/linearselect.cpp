75e817768fd880f08282f6da363f870ab350fb26
JehPark <qkrwpgjs1230@naver.com>
2021-05-15 20:24:18 +0900

    finished

--- chap05/linearSelection/linearselect.cpp
+++ chap05/linearSelection/linearselect.cpp
@@ -6,36 +6,36 @@ int linearSelect(int *arr, int p, int r, int i)
 	int j;
 
 	j = 0;
-	if (p < r)
+	if (r - p + 1 < 6)
+		return select(arr, p, r, i);
+	int size = (r - p + 1) / 5 + (r - p + 1) % 5 == 0 ? 0 : 1;
+	midarr = new int[size];
+	for (int i = p; i < r; i += 4)
 	{
-		if (r - p + 1 < 6)
-			return select(arr, p, r, i);
-		int size = (r - p + 1) / 5 + (r - p + 1) % 5 == 0 ? 0 : 1;
-		midarr = new int[size];
-		for (int i = p + 4; i <= r; i += 5)
+		if (size - 1 == j)
 		{
-			if (i > r)
-				midarr[j] = select(arr, i - 4, r, (i + r - 4) / 2);
-			midarr[j++] = select(arr, i - 4, i, (2 * i - 4) / 2);
+			midarr[j] = select(arr, i, r, (i + r) / 2);
+			break;
 		}
-		int M = linearSelect(midarr, 0, size - 1, j / 2);
-		delete[] midarr;
-		int q = partition(arr, 0, M, r);
-		int k = q - p + 1;
-		if (k > i)
-			return (linearSelect(arr, p, q - 1, i));
-		else if (k == i)
-			return (arr[q]);
-		else
-			return (linearSelect(arr, q + 1, r, i - k));
+		midarr[j++] = select(arr, i, i + 4, i + 2);
 	}
-	return (-1);
+	int M = linearSelect(midarr, 0, size - 1, size / 2);
+	delete[] midarr;
+	int q = partition(arr, 0, M, r);
+	int k = q - p + 1;
+	if (k > i)
+		return (linearSelect(arr, p, q - 1, i));
+	else if (k == i)
+		return (arr[q]);
+	else
+		return (linearSelect(arr, q + 1, r, i - k));
 }
 
 int select(int *arr, int p, int r, int i)
 {
 	int q, k;
-
+	if (p > r)
+		return (0);
 	if (p == r)
 		return (arr[p]);
 	q = partition(arr, p, r);
@@ -66,7 +66,7 @@ int partition(int *arr, int p, int r)
 	tmp = arr[r];
 	arr[r] = arr[i + 1];
 	arr[i + 1] = tmp;
-	return (i);
+	return (i + 1);
 }
 
 int partition(int *arr, int p, int r, int size)
@@ -76,7 +76,7 @@ int partition(int *arr, int p, int r, int size)
 	i = p - 1;
 	for (int j = p; j <= size; j++)
 	{
-		if (arr[j] < r)
+		if (arr[j] <= r)
 		{
 			i++;
 			tmp = arr[i];
@@ -84,5 +84,5 @@ int partition(int *arr, int p, int r, int size)
 			arr[j] = tmp;
 		}
 	}
-	return (i);
+	return (i + 1);
 }
